946f196d931cdfcbcc6e37ef35475327cc9de8bf
Thomas Maranzatto <tmaran@eduroamprvnat-172-16-63-14.near.illinois.edu>
2023-02-04 12:14:10 -0600

    bugs in simulation, trying to get step-by-step simulation working

--- simulation.py
+++ simulation.py
@@ -1,23 +1,57 @@
+from random import shuffle
 
 class Airplane:
     def __init__(self, n_rows, n_cols):
         self.rows = n_rows
         self.cols = n_cols
 
-        self.seating = [[(i,j) for i in range(n_cols)] for j in range(n_rows)]
+        self.seating = [[0 for _ in range(n_cols)] for _ in range(n_rows)]
+        self.currentState = [[0 for _ in range(n_cols + 1)] for _ in range(n_rows)]
+
+    def rowHelper(self, currentRow, passenger):
+        #move people out of the way if they need to be
+        #for now just move the person to the seat they are assigned to
+        self.seating[passenger[0]][passenger[1]] = 1
+        
+
+    def next(self, boardQueue, aisle):
+
+        #to start, move one timestep at a time
+        #then we can generalize later
+        for currentRow,cell in reversed(list(enumerate(aisle))):
+            print(currentRow, cell)
+            if cell == None:
+                continue
+            if cell[0] == currentRow:
+                self.rowHelper(currentRow, cell)
+            if currentRow != self.rows - 1:
+                if aisle[currentRow + 1] == None:
+                    aisle[currentRow] = None
+                    aisle[currentRow + 1] = cell
+        if aisle[0] == None:
+            aisle[0] = boardQueue.pop(0)
+        return None
 
     def simulateBoarding(self, boardingProcedure):
-        return None
-    
+        boardQueue = boardingProcedure(self.rows, self.cols)
+        aisle = [None for _ in range(self.rows)]
+        totalTime = 0
+        for i in range(100):
+            self.next(boardQueue, aisle)
+            totalTime += 1
+            print(boardQueue)
+            print()
+            print(aisle)
+            print()
+            print()
+        return totalTime
 
-def randomBoarding(seatingArrangement):
-    from random import shuffle
-    #assume seatingArrangement is a 2D array of i-j pairs
-    #where i is the col, and j is the row
-    seats = [item for sublist in seatingArrangement for item in sublist]
+
+def boardRandom(n, m):
+    seats = [(i,j) for i in range(n) for j in range(m)]
+    
     shuffle(seats)
     return seats
 
-
-A = Airplane(100, 6)
-print(randomBoarding(A.seating))
+A = Airplane(4, 2)
+A.simulateBoarding(boardRandom)

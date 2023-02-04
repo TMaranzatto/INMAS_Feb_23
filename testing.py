757787b5c46c2ba6cd456a4539a3292113c280b8
jmalionek <jmalionek@gmail.com>
2023-02-04 16:31:54 -0600

    Added some code which tests out the different algs

--- testing.py
+++ testing.py
@@ -1,48 +1,22 @@
 from simulation import Airplane
-import numpy as np
+from visulization import visulization
+from algorithms import *
+import matplotlib.pyplot as plt
 
-
-def test_next():
-	num_rows = 100
-	num_cols = 6
-	plane = Airplane(num_rows, num_cols)
-	boardingQ = [[(i,j) for i in range(num_cols)] for j in range(num_rows)]
-	aisleList = [None]*num_rows
-	aisleList.append(boardingQ.pop()
-	plane.next(boardingQ, aisle_list)
-	assert len(aisleList) == 1
-	plane.next(boardingQ, aisle_list)
-	assert Airplane.seating_matrix[0,0] == 1
-	assert (0,0) not in boardingQ and (0,0) not in aisleList
-	for i in range(10):
-		plane.next(boardingQ, aisle_list)
-		print(boardingQ, aisle_list)
-	
-def test_nearly_empty():
-	num_rows = 100
-	num_cols = 6
-	plane = Airplane(num_rows, num_cols)
-	boardingQ = [(50, i) for i in range(6)]
-	aisleList = [None]*num_rows
-	num_nexts = 0
-	too_long = False
-	while True:
-		plane.next(boardingQ, aisle_list)
-		num_nexts += 1
-		if num_nexts > 1_000_000:
-			too_long = True
-			break
-		if len(boardingQ)>0:
-			continue
-		for i in range(6):
-			if (50, i) in aisleList:
-			continue
-		break
-	assert not too_long
 
 def main():
-	test_nearly_empty()
-	test_next()
+	algs = [sinp, wma, randomBoarding, southwest, split, btf, ftb]
+	algs_names = ["sinp", "wma", "randomBoarding", "southwest", "split", "btf", "ftb"]
+	alg_times = []
+	for i, alg in enumerate(algs):
+		A = Airplane(40, 6)
+		alg_time = A.simulateBoarding(alg)
+		print(f'The {alg} algorithm took {alg_time}')
+		alg_times.append(alg_time)
+	
+	x = range(len(algs))
+	plt.xticks(x, algs)
+	plt.bar(range(len(algs)), alg_times, x_ticks = x)
 
 if __name__ == '__main__':
 	main()

46bac5945e3d9ac4d90ce090084da81daad90412
jmalionek <jmalionek@gmail.com>
2023-02-05 10:22:13 -0600

    did some graph stuff

--- create_graphs.py
+++ create_graphs.py
@@ -1,7 +1,7 @@
 
 import simulation
 import visulization
-import algorithms as algs
+from algorithms import *
 
 def create_gif(name, rows, cols, alg):
 	A = simulation.Airplane(rows, cols)
@@ -9,6 +9,8 @@ def create_gif(name, rows, cols, alg):
 	visulization.visulization(A.sateHistory, name)
 	
 
-
 if __name__ == '__main__':
-	create_gif('random.gif', 30, 6, algs.randomBoarding)
+	algs = [wma]
+	algs_names = ["wma"]
+	for i, alg in enumerate(algs):
+		create_gif(f'{algs_names[i]}.gif', 30, 6, alg)
